b2162869c47ca37bf61504edc6e3cd08d0f24ecc
xiaoyi0814 <1954742838@qq.com>
2025-03-31 16:40:50 +0800

    feature: defined and implemented Service data structure,which can cross threads

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,12 +1,66 @@
-use crate::{command_request::RequestData, CommandRequest, CommandResponse, KvError, Storage};
+use tracing::debug;
+
+use crate::{
+    command_request::RequestData, CommandRequest, CommandResponse, KvError, MemTable, Storage,
+};
+use std::sync::Arc;
 
 mod command_service;
 pub trait CommandService {
     fn execute(self, store: &impl Storage) -> CommandResponse;
 }
 
-// This function is used to dispatch the command to the corresponding service
+// the service data structure can cross threads,
+// and call `execute` to execute the CommandRequest command
+// and return CommandResponse
+pub struct Service<T = MemTable> {
+    inner: Arc<ServiceInner<T>>,
+}
 
+impl<T> Clone for Service<T> {
+    fn clone(&self) -> Self {
+        Self {
+            inner: Arc::clone(&self.inner),
+        }
+    }
+}
+
+impl<T: Storage> Service<T> {
+    pub fn new(store: T) -> Self {
+        Self {
+            inner: Arc::new(ServiceInner { store }),
+        }
+    }
+    pub fn execute(&self, cmd: CommandRequest) -> CommandResponse {
+        debug!("Got request: {:?}", cmd);
+        // TODO: send on_received event
+        let res = dispatch(cmd, &self.inner.store);
+        debug!("Executed response: {:?}", res);
+
+        // TODO: send on_executed event
+
+        res
+    }
+}
+
+pub struct ServiceInner<T> {
+    store: T,
+}
+
+impl<T: Storage> ServiceInner<T> {
+    pub fn new(store: T) -> Self {
+        Self { store }
+    }
+}
+
+impl<T: Storage> From<ServiceInner<T>> for Service<T> {
+    fn from(inner: ServiceInner<T>) -> Self {
+        Self {
+            inner: Arc::new(inner),
+        }
+    }
+}
+// This function is used to dispatch the command to the corresponding service
 pub fn dispatch(cmd: CommandRequest, store: &impl Storage) -> CommandResponse {
     match cmd.request_data {
         Some(RequestData::Hget(param)) => param.execute(store),
@@ -44,4 +98,22 @@ pub fn assert_res_err(res: &CommandResponse, code: u32, msg: &str) {
     assert!(res.message.contains(msg));
     assert_eq!(res.values, &[]);
     assert_eq!(res.pairs, &[]);
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[tokio::test]
+    async fn service_should_work() {
+        let service: Service = ServiceInner::new(MemTable::default()).into();
+        let cloned = service.clone();
+        tokio::spawn(async move {
+            let res = cloned.execute(CommandRequest::hset("t1", "hello", "world".into()));
+            assert_res_ok(&res, &[Value::default()], &[]);
+        })
+        .await
+        .unwrap();
+    }
 }
