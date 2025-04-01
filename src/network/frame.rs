67cbd3cbfa589517597bbdeb7b5048d0712ac836
xiaoyi0814 <1954742838@qq.com>
2025-04-01 22:58:01 +0800

    feature: written code to process frame, implementing the packet unpacking of network data streams

---
+++ src/network/frame.rs
@@ -0,0 +1,199 @@
+use std::io::Read;
+use std::io::Write;
+
+use crate::CommandRequest;
+use crate::CommandResponse;
+use crate::KvError;
+use bytes::Buf;
+use bytes::BufMut;
+use bytes::BytesMut;
+use flate2::read::GzDecoder;
+use flate2::{write::GzEncoder, Compression};
+use prost::Message;
+use tokio::io::{AsyncRead, AsyncReadExt};
+use tracing::debug;
+
+/// take the highest bit of HEAD_LEN as the flag of whether or not to compress
+pub const HIGH_LEN: usize = 4; // 4 bytes
+const MAX_FRAME: usize = 2 * 1024 * 1024 * 1024; // 2GB
+
+/// The MTU of Ethernet is 1500 bytes, and the IP header is 20 bytes, the TCP header is 20 bytes, so the maximum length of the TCP data is 1460 bytes.
+/// Generally, TCP packages will contain some Options(such as time stamp), so the maximum length of the TCP data is less than 1460 bytes. In that reserve 20 bytes for the Options, the maximum length of the TCP data is 1440 bytes.
+/// then subtract 4 bytes of length,which means 1436, the maximum message length without sharding.
+/// If the message is larger than 1436 bytes, it will be sharded into multiple frames.
+const COMPRESSION_LIMIT: usize = 1436;
+
+/// A bit flag used to mark the compressed data.
+const COMPRESSION_BIT: usize = 1 << 31;
+pub trait FrameCoder
+where
+    Self: Sized + Message + Default,
+{
+    // write to the passed `prost::Message` into a frame
+    fn encode_frame(&self, buf: &mut BytesMut) -> Result<(), KvError> {
+        let size = self.encoded_len();
+
+        if size > MAX_FRAME {
+            return Err(KvError::FrameError);
+        }
+
+        buf.put_u32(size as _);
+
+        // if the size is larger than the limit, we need to compress it
+        if size > COMPRESSION_LIMIT {
+            let mut buf1 = Vec::with_capacity(size);
+            self.encode(&mut buf1).unwrap();
+            let payload = buf.split_off(HIGH_LEN);
+            buf.clear();
+
+            // need to compress
+            let mut encoder = GzEncoder::new(payload.writer(), Compression::default());
+            encoder.write_all(&buf1[..])?;
+
+            // once the compression is done, we need to get the BytesMut back from the gzip encoder
+            let payload = encoder.finish()?.into_inner();
+            debug!("Encode a frame: size {}({})", size, payload.len());
+
+            buf.put_u32((payload.len() | COMPRESSION_BIT) as _);
+
+            // merge the payload back into the buffer
+            buf.unsplit(payload);
+
+            Ok(())
+        } else {
+            self.encode(buf).unwrap();
+            Ok(())
+        }
+    }
+    // decode a frame into the Message
+    fn decode_frame(buf: &mut BytesMut) -> Result<Self, KvError> {
+        let header = buf.get_u32() as usize;
+        let (len, compressed) = decode_header(header);
+        debug!("Got a frame: msg len {}, compressed: {}", len, compressed);
+
+        if compressed {
+            // decompress
+            let mut decoder = GzDecoder::new(&buf[..len]);
+            let mut buf1 = Vec::with_capacity(len * 2);
+            decoder.read_to_end(&mut buf1)?;
+            buf.advance(len);
+
+            Ok(Self::decode(&buf1[..buf1.len()])?)
+        } else {
+            let msg = Self::decode(&buf[..len])?;
+            buf.advance(len);
+            Ok(msg)
+        }
+    }
+}
+
+impl FrameCoder for CommandRequest {}
+impl FrameCoder for CommandResponse {}
+
+pub fn decode_header(header: usize) -> (usize, bool) {
+    let len = header & !COMPRESSION_BIT;
+    let compressed = header & COMPRESSION_BIT == COMPRESSION_BIT;
+    (len, compressed)
+}
+
+/// Read a complete frame from stream
+pub async fn read_frame<S>(stream: &mut S, buf: &mut BytesMut) -> Result<(), KvError>
+where
+    S: AsyncRead + Unpin + Send,
+{
+    let header = stream.read_u32().await? as usize;
+    let (len, _compressed) = decode_header(header);
+
+    // if there is no such memory, allocate at least on frame to make it available
+    buf.reserve(HIGH_LEN + len);
+    buf.put_u32(header as _);
+
+    unsafe { buf.advance_mut(len) };
+
+    stream.read_exact(&mut buf[HIGH_LEN..]).await?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Value;
+    use bytes::Bytes;
+
+    use super::*;
+
+    #[test]
+    fn command_request_encode_decode_should_work() {
+        let mut buf = BytesMut::new();
+
+        let cmd = CommandRequest::hdel("t1", "hello");
+        cmd.encode_frame(&mut buf).unwrap();
+
+        assert!(!is_compressed(&buf));
+
+        let res = CommandRequest::decode_frame(&mut buf).unwrap();
+        assert_eq!(cmd, res);
+    }
+
+    #[test]
+    fn command_response_encode_decode_should_work() {
+        let mut buf = BytesMut::new();
+        let values: Vec<Value> = vec![1.into(), "hello".into(), "world".into()];
+        let res: CommandResponse = values.into();
+        res.encode_frame(&mut buf).unwrap();
+
+        assert!(!is_compressed(&buf));
+
+        let result = CommandResponse::decode_frame(&mut buf).unwrap();
+        assert_eq!(res, result);
+    }
+
+    #[test]
+    fn command_response_compressed_encode_decode_should_work() {
+        let mut buf = BytesMut::new();
+        let value: Value = Bytes::from(vec![0u8; COMPRESSION_LIMIT + 1]).into();
+        let res: CommandResponse = value.into();
+        res.encode_frame(&mut buf).unwrap();
+
+        assert!(is_compressed(&buf));
+
+        let result = CommandResponse::decode_frame(&mut buf).unwrap();
+        assert_eq!(res, result);
+    }
+
+    fn is_compressed(data: &[u8]) -> bool {
+        let header = u32::from_be_bytes(data[..4].try_into().unwrap()) as usize;
+        header & COMPRESSION_BIT == COMPRESSION_BIT
+    }
+
+    struct DummyStream {
+        buf: BytesMut,
+    }
+
+    impl AsyncRead for DummyStream {
+        fn poll_read(
+            self: std::pin::Pin<&mut Self>,
+            _cx: &mut std::task::Context<'_>,
+            buf: &mut tokio::io::ReadBuf<'_>,
+        ) -> std::task::Poll<std::io::Result<()>> {
+            let len = buf.capacity();
+            let data = self.get_mut().buf.split_to(len);
+            buf.put_slice(&data);
+            std::task::Poll::Ready(Ok(()))
+        }
+    }
+
+    #[tokio::test]
+    async fn read_frame_should_work() {
+        let mut buf = BytesMut::new();
+        let cmd = CommandRequest::hdel("t1", "hello");
+        cmd.encode_frame(&mut buf).unwrap();
+
+        let mut stream = DummyStream { buf };
+        let mut data = BytesMut::new();
+
+        read_frame(&mut stream, &mut data).await.unwrap();
+
+        let res = CommandRequest::decode_frame(&mut data).unwrap();
+        assert_eq!(cmd, res);
+    }
+}
---
+++ src/network/mod.rs
@@ -0,0 +1,147 @@
+mod frame;
+
+use bytes::BytesMut;
+use tokio::io::{AsyncRead, AsyncWrite, AsyncWriteExt};
+use tracing::info;
+
+use crate::{CommandRequest, CommandResponse, KvError, Service};
+
+pub use frame::{read_frame, FrameCoder};
+
+/// Handle read and write of sockets from an accept on the server.
+pub struct ProstServerStream<S> {
+    inner: S,
+    service: Service,
+}
+
+/// Handle read and write client sockets.
+pub struct ProstClientStream<S> {
+    inner: S,
+}
+
+impl<S> ProstServerStream<S>
+where
+    S: AsyncRead + AsyncWrite + Unpin + Send,
+{
+    pub fn new(stream: S, service: Service) -> Self {
+        Self {
+            inner: stream,
+            service,
+        }
+    }
+
+    pub async fn process(mut self) -> Result<(), KvError> {
+        while let Ok(cmd) = self.recv().await {
+            info!("Got a new command: {:?}", cmd);
+            let res = self.service.execute(cmd);
+            self.send(res).await?;
+        }
+
+        Ok(())
+    }
+
+    async fn send(&mut self, msg: CommandResponse) -> Result<(), KvError> {
+        let mut buf = BytesMut::new();
+        msg.encode_frame(&mut buf)?;
+        let encoded = buf.freeze();
+        self.inner.write_all(&encoded[..]).await?;
+        Ok(())
+    }
+
+    async fn recv(&mut self) -> Result<CommandRequest, KvError> {
+        let mut buf = BytesMut::new();
+        let stream = &mut self.inner;
+        read_frame(stream, &mut buf).await?;
+        CommandRequest::decode_frame(&mut buf)
+    }
+}
+
+impl<S> ProstClientStream<S>
+where
+    S: AsyncRead + AsyncWrite + Unpin + Send,
+{
+    pub fn new(stream: S) -> Self {
+        Self { inner: stream }
+    }
+
+    pub async fn execute(&mut self, cmd: CommandRequest) -> Result<CommandResponse, KvError> {
+        self.send(cmd).await?;
+        self.recv().await
+    }
+
+    async fn send(&mut self, msg: CommandRequest) -> Result<(), KvError> {
+        let mut buf = BytesMut::new();
+        msg.encode_frame(&mut buf)?;
+        let encoded = buf.freeze();
+        self.inner.write_all(&encoded[..]).await?;
+        Ok(())
+    }
+
+    async fn recv(&mut self) -> Result<CommandResponse, KvError> {
+        let mut buf = BytesMut::new();
+        let stream = &mut self.inner;
+        read_frame(stream, &mut buf).await?;
+        CommandResponse::decode_frame(&mut buf)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::net::SocketAddr;
+
+    use bytes::Bytes;
+    use tokio::net::{TcpListener, TcpStream};
+
+    use crate::{assert_res_ok, MemTable, ServiceInner, Value};
+
+    use super::*;
+    #[tokio::test]
+    async fn client_server_basic_communication_should_work() -> anyhow::Result<()> {
+        let addr = start_server().await?;
+        let stream = TcpStream::connect(addr).await?;
+        let mut client = ProstClientStream::new(stream);
+
+        let cmd = CommandRequest::hset("t1", "hello", "world".into());
+        let res = client.execute(cmd).await?;
+        assert_res_ok(&res, &[Value::default()], &[]);
+
+        let cmd = CommandRequest::hget("t1", "hello");
+        let res = client.execute(cmd).await?;
+        assert_res_ok(&res, &["world".into()], &[]);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn client_server_compression_should_work() -> anyhow::Result<()> {
+        let addr = start_server().await?;
+        let stream = TcpStream::connect(addr).await?;
+        let mut client = ProstClientStream::new(stream);
+
+        let v: Value = Bytes::from(vec![0u8; 16384]).into();
+        let cmd = CommandRequest::hset("t2", "foo", v.clone());
+        let res = client.execute(cmd).await?;
+
+        assert_res_ok(&res, &[Value::default()], &[]);
+
+        let cmd = CommandRequest::hget("t2", "foo");
+        let res = client.execute(cmd).await?;
+
+        assert_res_ok(&res, &[v], &[]);
+
+        Ok(())
+    }
+
+    async fn start_server() -> anyhow::Result<SocketAddr> {
+        let listener = TcpListener::bind("127.0.0.1:0").await.unwrap();
+        let addr = listener.local_addr().unwrap();
+
+        tokio::spawn(async move {
+            let (stream, _) = listener.accept().await.unwrap();
+            let service: Service = ServiceInner::new(MemTable::new()).into();
+            let server = ProstServerStream::new(stream, service);
+            tokio::spawn(server.process());
+        });
+
+        Ok(addr)
+    }
+}
